0ef59e34e7eac9eb1426128d99d6a049392133e8
SadamRohith <sadamrohith456@gmail.com>
2025-06-13 15:30:19 -0400

    Removed Comments

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -3,7 +3,6 @@ import { Box } from "@chakra-ui/react";
 import { DistrictSearch } from "./District/DistrictSearch";
 
 export const Home: React.FC = () => {
-  // The outer Box no longer needs padding, as App.tsx handles it now.
   return (
     <Box width="100%">
       <DistrictSearch />
